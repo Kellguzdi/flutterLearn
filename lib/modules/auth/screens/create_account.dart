811a9033e546570e34ec4d34e5bd1ce713f723a0
Kellguzdi <20213tn002@utez.edu.mx>
2024-10-23 20:23:13 -0600

    consume firestore

--- lib/modules/auth/screens/create_account.dart
+++ lib/modules/auth/screens/create_account.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
 
@@ -7,76 +9,89 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    final TextEditingController _emailController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
-    final TextEditingController _confirmController = TextEditingController();
-    bool _isObscure = true;
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: const Text('Registrate'),
-                centerTitle: true,
-                backgroundColor: Colors.pink,
-                titleTextStyle: const TextStyle(fontSize: 20, color: Colors.white),
-            ),
-            body: SingleChildScrollView(
-                child:Padding(
-                    padding:EdgeInsets.all(16.0),
-                    child: Center(
-                        child:Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children:[
-                                Image.asset('assets/logo.png',width:200,height:200),
-                                Form(
-                                    key:_formKey,
-                                    child: Column(
-                                        children:[
-                                            TextFormField(
-                                            controller: _emailController,
-                                            decoration: InputDecoration(
-                                                label: Text('Correo electrónico')
-                                            )
-                                            ),
-                                            SizedBox(
-                                                height:16
-                                            ),
-                                            TextFormField(
-                                                controller:_passwordController,
-                                                obscureText: _isObscure,
-                                                decoration: InputDecoration(
-                                                    label: Text('Contraseña'),
-                                                )
-                                            ),
-                                            SizedBox(
-                                                height:16
-                                            ),  
-                                            TextFormField(
-                                                controller:_confirmController,
-                                                obscureText: _isObscure,
-                                                decoration: InputDecoration(
-                                                    label: Text('Confirmar Contraseña'),
-                                                    suffixIcon: IconButton(
-                                                        onPressed: () {
-                                                        setState(() {
-                                                            _isObscure = !_isObscure;
-                                                        });
-                                                        },
-                                                        icon: Icon(_isObscure
-                                                            ? Icons.visibility
-                                                            : Icons.visibility_off)
-                                                    )
-                                                )
-                                            )
-                                        ]
-                                    )
-                                )
-                            ]
-                        )
-                    )
-                )
-            )
-        );
-    }
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmController = TextEditingController();
+  bool _isObscure = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Registrate'),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          titleTextStyle: const TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Center(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                      Image.asset('assets/logo.png', width: 200, height: 200),
+                      Form(
+                          key: _formKey,
+                          child: Column(children: [
+                            TextFormField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                    label: Text('Correo electrónico'))),
+                            SizedBox(height: 16),
+                            TextFormField(
+                                controller: _passwordController,
+                                obscureText: _isObscure,
+                                decoration: InputDecoration(
+                                  label: Text('Contraseña'),
+                                )),
+                            SizedBox(height: 16),
+                            TextFormField(
+                                controller: _confirmController,
+                                obscureText: _isObscure,
+                                decoration: InputDecoration(
+                                    label: Text('Confirmar Contraseña'),
+                                    suffixIcon: IconButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            _isObscure = !_isObscure;
+                                          });
+                                        },
+                                        icon: Icon(_isObscure
+                                            ? Icons.visibility
+                                            : Icons.visibility_off)))),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 48,
+                              child: ElevatedButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      try {
+                                        final credential = await FirebaseAuth
+                                            .instance
+                                            .createUserWithEmailAndPassword(
+                                          email: _emailController.text,
+                                          password: _passwordController.text,
+                                        );
+                                        print(credential);
+                                      } on FirebaseAuthException catch (e) {
+                                        if (e.code == 'user-not-found') {
+                                          print(
+                                              'No user found for that email.');
+                                        } else if (e.code == 'wrong-password') {
+                                          print(
+                                              'Wrong password provided for that user.');
+                                        }
+                                      }
+                                    }
+                                  },
+                                  style: const ButtonStyle(),
+                                  child: const Text('Crear cuenta')),
+                            ),
+                          ]))
+                    ])))));
+  }
 }
--- lib/navigation/home.dart
+++ lib/navigation/home.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating/flutter_rating.dart';
+import 'package:learning/modules/restaurant/entities/restaurant.dart';
 //crear un stateful widget
 //Dentro del widget crear la instancia con final
 
@@ -16,23 +19,87 @@ import 'package:flutter/material.dart';
 //}
 
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({
     super.key,
   });
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _isLoading = true;
+  List<Restaurant>  restaurants = [];
+
+  final db = FirebaseFirestore.instance;
+  @override
+  void initState() {
+    super.initState();
+    (() async=> {
+      await db.collection("restaurants").get().then((event) {
+      for (var doc in event.docs) {
+        final restaurant = Restaurant(doc.data()['name'], doc.data()['description'], List<String>.from(doc.data()['images']), doc.data()['rating'], doc.data()['count']);
+        restaurants.add(restaurant);
+      }
+      if(mounted){
+        setState(() {
+          _isLoading = false;
+        });
+      }
+      })
+    }) ();
+
+  } 
+  
+
+  @override
   Widget build(BuildContext context) {
     //Es semejante al safeArea o flex:1 en React Native
     //Dicta que sea en desarrollo móvil y permite colocar fondos de pantalla
-
+    if(_isLoading){
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
     return Scaffold(
       //Es recomendable tenerlo para cada vista
       appBar: AppBar(
         title: const Text('Inicio'),
       ),
-      body: const Center(
-        child: Text('Inicio'),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Image.network(restaurants[0].images[0],width: 60, height: 60,),
+              const SizedBox(
+                width: 8,
+              ),
+              Column(
+                children: [
+                  Text(restaurants[0].name, style: const TextStyle(fontSize: 20),),
+                  SizedBox(
+                    width: 200,
+                    height: 100,
+                    child: Text(
+                      restaurants[0].description,
+                    ),
+                  )
+                ],
+              ),
+              const Spacer(),
+              StarRating(
+                rating: restaurants[0].rating / restaurants[0].count,
+                color: Colors.orange,
+                borderColor: Colors.grey,
+                starCount: 5,
+                size: 12,
+              )
+            ],
+          )
+        ]
       ),
       //floating esta fuera de body por ser propiedad de scaffold
       floatingActionButton: FloatingActionButton(
@@ -45,4 +112,5 @@ class Home extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+
 }
