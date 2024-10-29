b8833b78130295f51fd2d44236bd516ec9585f15
Abraham <20213tn114@utez.edu.mx>
2024-10-28 18:57:36 -0600

    practica

--- lib/modules/restaurant/widgets/custom_list_restaurants.dart
+++ lib/modules/restaurant/widgets/custom_list_restaurants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_rating/flutter_rating.dart';
 import 'package:learning/modules/restaurant/entities/restaurant.dart';
-
+import 'package:learning/modules/restaurant/widgets/restaurant_details.dart';
 class CustomListRestaurants extends StatelessWidget {
   const CustomListRestaurants({
     super.key,
@@ -13,42 +13,60 @@ class CustomListRestaurants extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Image.network(
-          restaurant.images[0],
-          width: 60,
-          height: 60,
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              restaurant.name,
-              style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              width: 200,
-              height: 40,
-              child: Text(
-                restaurant.description,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return RestaurantDetails(
+                images: restaurant.images,
+                name: restaurant.name,
+                description: restaurant.description,
+                rating: restaurant.rating,
+                count: restaurant.count,
+              );
+            },
+          ),
+        );
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            restaurant.images[0],
+            width: 60,
+            height: 60,
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                restaurant.name,
+                style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
               ),
-            )
-          ],
-        ),
-        const Spacer(),
-        StarRating(
-          rating: restaurant.rating / restaurant.count,
-          color: Colors.orange,
-          borderColor: Colors.grey,
-          starCount: 5,
-          size: 12,
-        )
-      ],
+              SizedBox(
+                width: 200,
+                height: 40,
+                child: Text(
+                  restaurant.description,
+                ),
+              )
+            ],
+          ),
+          const Spacer(),
+          StarRating(
+            rating: restaurant.rating / restaurant.count,
+            color: Colors.orange,
+            borderColor: Colors.grey,
+            starCount: 5,
+            size: 12,
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/modules/restaurant/widgets/restaurant_details.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_rating/flutter_rating.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+class RestaurantDetails extends StatelessWidget {
+  const RestaurantDetails({
+    required this.name,
+    required this.description,
+    required this.images,
+    required this.rating,
+    required this.count,
+    super.key,
+  });
+  final String name;
+  final String description;
+  final List<String> images;
+  final double rating;
+  final int count;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title:Text(name)
+        ),
+        body: Center(
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                    children:[
+                        CarouselSlider(
+                            options: CarouselOptions(height: 300.0),
+                            items: images.map((i){
+                                return Builder(
+                                    builder:( BuildContext context){
+                                        return Container(
+                                            width: MediaQuery.of(context).size.width,
+                                            margin: EdgeInsets.symmetric(horizontal: 5.0),        
+                                            child: Image.network(
+                                                i, 
+                                                fit: BoxFit.cover, 
+                                            ),
+                                        );
+                                    }
+                                );
+                            }).toList(),
+                        ),
+                        SizedBox(height:20),
+                        Row(
+                            children:[
+                                Text(name,style:TextStyle(fontWeight:FontWeight.bold)),
+                                Expanded(
+                                    child: Column()
+                                ),
+                                StarRating(
+                                    rating: rating / count,
+                                    color: Colors.orange,
+                                    borderColor: Colors.grey,
+                                    starCount: 5,
+                                    size: 12,
+                                )
+                            ]
+                        ),
+                        SizedBox(
+                            height:20
+                        ),
+                        SizedBox(
+                            width:double.infinity,
+                            child: Text(description)
+                        )
+                    ]
+                ),
+            )
+        ),
+    );
+  }
+}
